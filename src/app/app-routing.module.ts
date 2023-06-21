64faa9a5b4f1a5aa9c36b427355f02f3425c8297
yrhwang <yrhwang@softnet.co.kr>
2023-06-21 16:26:36 +0900

    echart1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsComponent } from "./charts/charts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ChartsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.scss']
 })
-export class ChartsComponent {
+export class ChartsComponent implements AfterViewInit {
+  @ViewChild('chartContainer', { static: false }) chartContainer!: ElementRef;
 
+  ngAfterViewInit(): void {
+    const chart = echarts.init(this.chartContainer.nativeElement);
+
+    const option = {
+      title: {
+        text: 'Lifelog Radar Chart',
+        textStyle: {
+          color: '#333',
+          fontSize: 18,
+          fontWeight: 'bold'
+        }
+      },
+      tooltip: {},
+      legend: {
+        data: ['test1'],
+        textStyle: {
+          color: '#666',
+          fontSize: 14
+        }
+      },
+      radar: {
+        indicator: [
+          { name: 'temp (°C)', max: 100 },
+          { name: 'pulse (bpm)', max: 200 },
+          { name: 'breath (bpm)', max: 50 },
+          { name: 'oxygen (%)', max: 100 },
+          { name: 'step', max: 10000 },
+          { name: 'stress', max: 100 }
+        ],
+        splitArea: {
+          show: true,
+          areaStyle: {
+            color: ['rgba(250, 250, 250, 0.6)', 'rgba(240, 240, 240, 0.6)']
+          }
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: '#bbb',
+            width: 1
+          }
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#ddd',
+            width: 1
+          }
+        }
+      },
+      series: [
+        {
+          name: 'lifelog',
+          type: 'radar',
+          data: [
+            {
+              value: [37.2, 80, 12, 98, 8000, 60],
+              name: 'test1',
+              areaStyle: {
+                color: 'rgb(137, 207, 240)'
+              },
+              lineStyle: {
+                color: '#1f7ebd',
+                width: 2
+              },
+              symbol: 'circle',
+              symbolSize: 6,
+              itemStyle: {
+                color: '#1f7ebd'
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+    chart.setOption(option);
+  }
 }
